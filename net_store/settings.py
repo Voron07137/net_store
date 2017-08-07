2e64fe0b3b9ae937a1102f819e86c3e70855b311
Voron07137 <oleg07137@gmail.com>
2017-08-08 02:14:33 +0300

    tyujtyj

--- net_store/settings.py
+++ net_store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn/")
+
+MEDIA_URL = '/media_cdn/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn/")
